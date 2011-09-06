536aa1e4b760940785c8933ee9734c67b73e369c
Don Pottinger <don.pottinger@gmail.com>
2011-09-06 16:05:49 -0400

    Token authentication example implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 TokenApp::Application.routes.draw do
   get "home/index"
 
-  devise_for :users
+#  devise_for :users, :controllers => { :sessions => "users/sessions" }
+  
+  devise_for :users, :controllers => { :sessions => "sessions" }
+
+  devise_scope :user do
+    resources :sessions, :only => [:create, :destroy]
+  end
   
   resources :token_authentications, :only => [:create, :destroy]
 
